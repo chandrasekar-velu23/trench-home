e722a4c993140c87c0295c64787f496cf5442b22
chandrasekar-velu23 <chandrasekarvelu23@gmail.com>
2026-06-24 22:48:00 +0530

    google tag manager  code updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,6 +117,16 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P4DNDLW4');
+          `}
+        </Script>
+
         {/* Load Google Tag Library */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-E1SQD3N78D"
@@ -139,6 +149,14 @@ export default function RootLayout({
         className="antialiased font-secondary"
         suppressHydrationWarning
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P4DNDLW4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Analytics />
         <LenisProvider>
           <NavigationLoaderProvider>
